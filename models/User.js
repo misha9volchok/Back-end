6878df38e2bfab1fd75fc280259dfd3c4b5b5ce8
Volchok <michael.volchok9@gmail.com>
2021-08-14 15:58:40 +0300

    Register API
    Added registration API to User route

--- models/User.js
+++ models/User.js
@@ -1,47 +1,51 @@
 const mongoose = require('mongoose')
 
-const schema = mongoose.Schema({
-  user_name: String,
-  password: String,
-  Email: String,
-  Dog: [
-    {
-      name: String,
-      sex: String,
-      weight: String,
-      age: Number,
-      Breed: [
-        {
-          Name: String,
-          Origin: String,
-          Breeding: String,
-          LifeSpam: [String],
-          Height: [String],
-          weight: [String],
-          HealthIssues: Boolean,
-          Trainability: String,
-          EasyTraining: Boolean,
-          Intelligence: Number
-        }
-      ],
-      allergy: [String],
-      vaccines: [
-        {
-          name: String,
-          date: Date
-        }
-      ],
-      food: [
-        {
-          food_name: String,
-          ingredient_list: [String],
-          food_amount_grams: Number,
-          calories_grams: Number
-        }
-      ]
-    }]
-},{
+const schema = mongoose.Schema(
+  {
+    user_name: { type: String, required: true },
+    password: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Dog: [
+      {
+        name: String,
+        sex: String,
+        weight: String,
+        age: Number,
+        Breed: [
+          {
+            Name: String,
+            Origin: String,
+            Breeding: String,
+            LifeSpam: [String],
+            Height: [String],
+            weight: [String],
+            HealthIssues: Boolean,
+            Trainability: String,
+            EasyTraining: Boolean,
+            Intelligence: Number
+          }
+        ],
+        allergy: [String],
+        vaccines: [
+          {
+            name: String,
+            date: Date
+          }
+        ],
+        food: [
+          {
+            food_name: String,
+            ingredient_list: [String],
+            food_amount_grams: Number,
+            calories_grams: Number
+          }
+        ]
+      }
+    ]
+  },
+  {
     collection: 'User'
-})
+  }
+)
 
 module.exports = mongoose.model('User', schema)
--- route/User.js
+++ route/User.js
@@ -1,35 +1,54 @@
 const express = require('express')
-const User = require("../models/User") // new
+const User = require('../models/User')
+//const bcrypt = require('bcrypt')
 const router = express.Router()
 
 // Get all posts
 
 router.get('/user', async (req, res) => {
-    const user = await User.find().select('Dog')
-   
-    res.send(user)
+  const user = await User.find().select('Dog')
+
+  res.send(user)
 })
 
-router.post("/user", async (req, res)=>{
-    const user = new User({
+router.post('/user', async (req, res) => {
+  const user = new User({
+    user_name: req.body.user_name,
+    password: req.body.password
+  })
+
+  await post.save()
+  res.send(post)
+})
+
+router.get('/user/:id', async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id })
+    res.send(user)
+  } catch {
+    res.status(404)
+    res.send({ error: "User doesn't exist!" })
+  }
+})
+// ##### CREATE NEW USER ##### //
+
+router.post('/register', async (req, res) => {
+  // Check email not registered
+  await User.findOne({ Email: req.body.Email }).then(user => {
+    if (user) {
+      // return 400 if email exists
+      return res.status(400).json({ Email: 'Alredy exists' })
+    } else {
+      // Create new User
+      const newUser = new User({
         user_name: req.body.user_name,
         password: req.body.password,
-        Email:req.body.Email,
-        Dog:req.body.Dog
-    })
-
-    await post.save()
-    res.send(post)
-})
-
-
-router.get("/user/:id", async (req, res)=> {
-    try {
-        const user = await User.findOne({_id: req.params.id})
-        res.send(user)
-    } catch {
-        res.status(404)
-        res.send({ error: "User doesn't exist!"})
+        Email: req.body.Email
+      })
+      newUser.save()
+      return res.status(200).json({ msg: newUser })
     }
+  })
 })
+
 module.exports = router
