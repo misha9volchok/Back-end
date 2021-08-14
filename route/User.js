e990718fd199f70e6ba5e15b93c311b587c53e18
Volchok <michael.volchok9@gmail.com>
2021-08-14 18:00:58 +0300

    Update User.js
    Add /login function

--- route/User.js
+++ route/User.js
@@ -51,4 +51,26 @@ router.post('/register', async (req, res) => {
   })
 })
 
+// ###### LOGIN ###### //
+
+router.post('/login', function(req, res) {
+    const user_name  = req.body.user_name;
+    const password = req.body.password;
+
+    User.findOne({user_name: user_name, password:password}, function(err, user){
+        if(err) {
+            console.log(err);
+            return res.status(500).send({error: "send request error"});
+        }
+
+        if (!user) {
+            return res.status(404).send({error: "User doest not exist"});
+        }
+
+        return res.status(200).send(user_name);
+    })
+
+
+})
+
 module.exports = router
