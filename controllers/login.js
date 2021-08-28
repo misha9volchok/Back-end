4f8a8f499753f7728d3bdbfc14a171904c0fb2bc
Volchok <michael.volchok9@gmail.com>
2021-08-28 19:46:46 +0300

    added decrypt password

--- controllers/login.js
+++ controllers/login.js
@@ -1,22 +1,26 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 exports.login = (req, res) => {
-    const user_name = req.body.user_name
-    const password = req.body.password
-  
-    User.findOne({ user_name: user_name, password: password }, function (
-      err,
-      user
-    ) {
+  const user_name = req.body.user_name
+  const password = req.body.password
+
+  User.findOne({ user_name: user_name}, function (
+    err,
+    user
+  ) {
+    bcrypt.compare(req.body.password, password, (err, result) => {
       if (err) {
         console.log(err)
         return res.status(500).send({ error: 'send request error' })
       }
-  
+
       if (!user) {
         return res.status(400).send({ error: 'User doest not exist' })
       }
-  
+
       return res.status(200).json({ user_name: user_name })
     })
-  }
+  })
+}
