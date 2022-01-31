ee72be6b9ce7b1d05cb2410d88bca4214e88ace3
Volchok <michael.volchok9@gmail.com>
2022-01-31 18:13:06 +0200

    query changed back to body

--- controllers/register.js
+++ controllers/register.js
@@ -1,24 +1,25 @@
 const User = require('../models/User')
+const Dog = require('../models/Dog')
 const bcrypt = require('bcrypt')
 const mongoose = require("mongoose");
 
 exports.register = async (req, res) => {
   // Check email not registered
-  await User.findOne({ Email: req.query.Email }).then(user => {
+  await User.findOne({ Email: req.body.Email }).then(user => {
     if (user) {
       // return 400 if email exists
       return res.status(400).json({ Email: 'Alredy exists' })
     } else {
-      bcrypt.hash(req.query.password, 10, (err, hash) => {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
         if (err) {
           return res.status(500).json({
             error: err
           })
         } else {
           const newUser = new User({
-            user_name: req.query.user_name,
+            user_name: req.body.user_name,
             password: hash,
-            Email: req.query.Email,
+            Email: req.body.Email,
             Dog: { dogIndex: 0, name: '', sex: '', age: 0 }
           })
           newUser.save()
